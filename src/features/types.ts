cd286db76fa57541ee32a0ad2f4043a9a1c8eb64
Rujuu-prog <n.ujiie2118@gmail.com>
2021-05-06 01:36:17 +0900

    first commit

---
+++ src/features/types.ts
@@ -0,0 +1,47 @@
+//fileオブジェクトの型
+export interface File extends Blob {
+    readonly lastModified: number;
+    readonly name: string;
+}
+/*authSlice.ts*/
+export interface PROPS_AUTHEN {
+    email: string;
+    password: string;
+}
+
+export interface PROPS_PROFILE {
+    id: number;
+    nickName: string;
+    //imgがない場合もあるので、ユニオンでnullも指定
+    img: File | null;
+}
+
+export interface PROPS_NICKNAME {
+    nickName: string;
+}
+
+/*postSlice.ts*/
+export interface PROPS_NEWPOST {
+    title: string;
+    //imgがない場合もあるので、ユニオンでnullも指定
+    img: File | null;
+}
+export interface PROPS_LIKED {
+    id: number;
+    title: string;
+    current: number[];
+    new: number;
+}
+export interface PROPS_COMMENT {
+    text: string;
+    post: number;
+}
+/*Post.tsx*/
+export interface PROPS_POST {
+    postId: number;
+    loginId: number;
+    userPost: number;
+    title: string;
+    imageUrl: string;
+    liked: number[];
+}
