ad83f8ac6272f3a239cb1b89937aed372beb86c2
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2021-05-16 01:29:02 +0900

    add async function

---
+++ .history/src/features/auth/authSlice_20210506013531.ts
@@ -0,0 +1,53 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+import axios from 'axios';
+import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from '../types';
+//djangoのurlをenvから取得
+const apiUrl = process.env.REACT_APP_DEV_API_URL;
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        //login用のモーダルのtrue false
+        openSignIn: true,
+        //register用のモーダルのtrue false
+        openSignUp: false,
+        //profiel変更用のモーダルのtrue false
+        openProfile: false,
+        //バックエンドの処理中のローディングのtrue false？
+        isLoadingAuth: false,
+        myprofile: {
+            id: 0,
+            nickName: "",
+            userProfile: 0,
+            created_on: "",
+            img: "",
+        },
+        //apiで取得してきたprofileのリストを保持しておくためのオブジェクト
+        profiles: [
+            {
+                id: 0,
+                nickName: "",
+                userProfile: 0,
+                created_on: "",
+                img: "",
+            },
+        ],
+    },
+  // The `reducers` field lets us define reducers and generate associated actions
+    reducers: {
+        //fetchの開始時と終了時の処理
+        fetchCredStart(state) {
+            state.isLoadingAuth = true;
+        },
+        fetchCredEnd(state) {
+            state.isLoadingAuth = false;
+        },
+    },
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export const selectCount = (state: RootState) => state.counter.value;
+
+export default counterSlice.reducer;
